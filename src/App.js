38b6f27e6d7b8b760eca76625adaf11a3a5f37aa
Nikita George <nikkigeorge2@gmail.com>
2022-03-26 16:01:24 -0600

    added action, reducer, and store to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 import { robots } from './robots';
 import './App.css';
+import { setSearchField } from './actions';
 
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (e) => dispatch(setSearchField(e.target.value))
+	}
+}
 class App extends Component {
     constructor() {    //constructor  creates this.
         super()
         this.state = {
-            robots: robots,
-            searchfield: ''
+            robots: robots
         }
      }
 
@@ -21,22 +33,26 @@ class App extends Component {
     
  //Searchbox (type the name you want) 
  //onSearchChange lets the server know there is a change   
-onSearchChange = (event) => {     
-    this.setState({ searchfield: event.target.value })
-}
+// onSearchChange = (event) => {     
+//     this.setState({ searchfield: event.target.value })
+// }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+				const { robots } = this.state;
+				const { searchField, onSearchChange } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-    return (
+    return !robots.length ?
+		<h1>Loading</h1> :
+		(
         <div className='tc'>
             <h1>Robofriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>   
+            <SearchBox searchChange={onSearchChange}/>   
             <CardList robots={filteredRobots}/>
         </div>
     );
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux'; 
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
